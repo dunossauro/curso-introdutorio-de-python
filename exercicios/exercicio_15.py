811508e09edd1b347163a159e875bb1759d13a21
Josue23 <josuerodrigues@gmail.com>
2022-09-15 21:10:44 -0300

    Exercício 15

--- exercicios/exercicio_15.py
+++ exercicios/exercicio_15.py
@@ -1,6 +1,9 @@
 '''
 Faça um programa que dada a entrada de uma lista o programa calcule a
 combinatória de dois elementos e nos retorne as combinações em uma nova lista.
+
+Exemplo de entrada: [1, 2, 3, 4]
+Exemplo de saída: [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 '''
 
 
